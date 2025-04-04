c4eb57357d41b49faef0478c06efe2d885fb0037
Layssa <layglez@hotmail.com>
2025-04-03 23:46:47 -0600

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import About_me from './pages/About_me';
 import './App.css';
+import Navbar from './componentes/Navbar';
 
-function App() {}
+function App() {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path='projects' element={<Projects />} />
+                <Route path='contact' element={<Contact />} />
+                <Route path='about_me' element={<About_me />} />
+            </Routes>
+        </>
+    );
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// BrowserRouter es un componente interactua y mantiene en
+// memoria la historia del navegador para que puedas tener
+// diferentes paginas en React
+// Siempre tiene que envolver tu aplicacion con el
+import { BrowserRouter } from 'react-router';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+);
